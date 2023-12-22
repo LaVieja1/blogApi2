0f555949f329741de119c2d152988f5c9ad1383d
santicano <canosantiago404@gmail.com>
2023-12-22 20:07:53 -0300

    usado bcryptjs para las contraseñas

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,26 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    //Check if user is already in the database
+    const nameExist = await User.findOne({ name: req.body.name });
+    if(nameExist) return res.status(400).send('Usuario ya existe');
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create new user
     const user = new User({
         name: req.body.name,
-        password: req.body.password
+        password: hashedPassword
     });
-
     try {
         const savedUser = await user.save();
         res.send(savedUser);
