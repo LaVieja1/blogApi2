040e9845787b4743886eba89804cb2a5cb094388
santicano <canosantiago404@gmail.com>
2023-12-22 20:28:37 -0300

    ruta de login exitosa

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const User = require('../models/User');
 const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 
-
+//REGISTER
 router.post('/register', async (req, res) => {
+    //VALIDATE USER
     const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
@@ -23,10 +24,26 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user._id });
     } catch(err) {
         res.status(400).send(err);
     }
+});
+
+//LOGIN
+router.post('/login', async (req,res) => {
+    //VALIDATE USER
+    const { error } = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Check if user is already in the database
+    const user = await User.findOne({ name: req.body.name });
+    if(!user) return res.status(400).send('Usuario no existe');
+    //Check password
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Contraseña incorrecta');
+
+    res.send('Login exitoso!');
 });
 
 module.exports = router;
